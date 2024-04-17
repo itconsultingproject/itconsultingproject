383e65ecf33d2c115645bd989dc41d405e8aa099
eucarystr <148537605+eucarystr@users.noreply.github.com>
2024-04-17 22:38:48 +0100

    Add files via upload

---
+++ src/components/About/about.js
@@ -0,0 +1,13 @@
+// AboutUs.js
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            {/* Add about us information here */}
+        </div>
+    );
+}
+
+export default AboutUs;
---
+++ src/components/Services/services.js
@@ -0,0 +1,13 @@
+// Services.js
+import React from 'react';
+
+function Services() {
+    return (
+        <div>
+            <h1>Our Services</h1>
+            {/* Add service information here */}
+        </div>
+    );
+}
+
+export default Services;
