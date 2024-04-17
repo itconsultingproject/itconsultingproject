ae07ef9e3278ef61f4ee03d07aeac131cd1f1cec
eucarystr <148537605+eucarystr@users.noreply.github.com>
2024-04-17 23:03:09 +0100

    Update about.js

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,13 +1,45 @@
 // AboutUs.js
 import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function AboutUs() {
     return (
-        <div>
+        <Container>
             <h1>About Us</h1>
-            {/* Add about us information here */}
-        </div>
+            <Row>
+                <Col md={6}>
+                    <Card>
+                        <Card.Img variant="top" src="about.jpg" />
+                        <Card.Body>
+                            <Card.Title>About Our Company</Card.Title>
+                            <Card.Text>
+                                Description about our company.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={6}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Contact Us</Card.Title>
+                            <Card.Text>
+                                Phone: 123-456-7890<br />
+                                Email: info@example.com
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Img variant="bottom" src="contact.jpg" />
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <h2>Clients We've Worked With</h2>
+                    {/* Add logos of companies here */}
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
 export default AboutUs;
+
